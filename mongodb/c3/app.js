7b716324d549b5977796b6cce600c67d67d93f73
Dalibor Antovski <d.antovski@gmail.com>
2023-04-06 19:37:57 +0200

    c3novo

--- mongodb/c3/app.js
+++ mongodb/c3/app.js
@@ -1,40 +1,53 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
+const express = require("express");
+const dotenv = require("dotenv");
+const movies = require("./handlers/movies");
+const connectToMongoDB = require("./pkg/db/index");
+const morgan = require("morgan");
 
 const app = express();
-app.use(express.urlencoded ({ extended: true }));
+
+// Midelware funkcii sto bi se izvrsuvale pred responsot
 app.use(express.json());
+// const logRequest = (req, res, next) => {
+//   console.log(`${req.method} ${req.url}`);
+//   next();
+// };
+// app.use(logRequest);
+app.use(morgan("dev"));
 
-const logRequest = (req, res, next) => {
-  console.log(`${req.method} ${req.url}`); next();
-};
+// Instaliranje na dotenv // npm i dotenv
+// so ova konfigurirame da se vcita config.env fajlot
+dotenv.config({ path: "./config.env" });
+// proveruvame dali imame pristap do config.env informaciite
+// console.log(process.env);
 
+// const DB = process.env.DATABASE.replace(
+//   "<PASSWORD>",
+//   process.env.DATABASE_PASSWORD
+// );
 
+// mongoose
+//   .connect(DB, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => {
+//     console.log("successfull conection to our DATABASE");
+//   })
+//   .catch((err) => console.log(err));
 
-app.use(logRequest);
+connectToMongoDB.init();
 
-//INSTALIRANJE NA DOTENV // NPM I DOTENV
+app.get("/movies", movies.getAll);
+app.get("/movies/:id", movies.getOne);
+app.post("/movies", movies.create);
+app.put("/movies/:id", movies.replace);
+app.patch("/movies/:id", movies.update);
+app.delete("/movies/:id", movies.delete);
 
-dotenv.config({path: './config.env'});
-
-const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
-
-mongoose.connect(DB, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}
-)
-.then(() => {
- console.log("uspesno se povrzavme");
-})
-.catch((err) => {
- console.log(err);
-});
-
-//console.log(process.env);
-
-const port = process.env.PORT;
-app.listen(port, () => {
-  console.log(`App running on port ${port}`);
+app.listen(process.env.PORT, (err) => {
+  if (err) {
+    return console.log("Could not start service");
+  }
+  console.log("Service started successfully on port 10000");
 });
