d64c6b4b339a0682ddfc884393fa673ca15b2db5
Dalibor Antovski <d.antovski@gmail.com>
2023-04-11 22:15:36 +0200

    cas5

--- mongodb/c4/handler/authHandler.js
+++ mongodb/c4/handler/authHandler.js
@@ -1,4 +1,6 @@
 const User = require("../pkg/user/userSchema");
+const jwt = require('jsonwebtoken');
+const bcrypt = require("bcryptjs");
 
 exports.signup = async (req, res) => {
   try {
@@ -8,13 +10,54 @@ exports.signup = async (req, res) => {
       password: req.body.password,
     });
 
+    const token = jwt.sign({id: newUser._id}, process.env.JWT_SECRET, {
+      expiresIn: process.env.JWT_EXPIRES,
+    });
+
     res.status(201).json({
       status: "success",
+      token,
       data: {
         user: newUser,
       },
     });
   } catch (err) {
+    return res.status(500).send(err);
+  }
+};
+
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    console.log(email, password);
+    //1.Proveruvame dali ima vneseno email i pasvord
+    if(!email || !password) {
+      return res.status(400).send('Please provide email and password!');
+    }
+    //2.Proveruvame dali korisnikot postoi
+    const user = await User.findOne({ email });
+    console.log(user);
+    if (!user) {
+      return res.status(401).send('Invalid email')
+    }
+    //3.Sporeduvame pasvordi
+    const isPasswordValid = bcrypt.compareSync(password, user.password);
+    console.log(isPasswordValid);
+    if (!isPasswordValid) {
+      return res.status(400).send('Invalid email or password!');
+    }
+    //4.Generirame i isprakjame token
+    const token = jwt.sign({id: user._id}, process.env.JWT_SECRET, {
+      expiresIn: process.env.JWT_EXPIRES,
+    });
+    res.status(201).json({
+      status: "success",
+      token,
+      //data: {
+      //  user,
+      //}
+    });
+  }catch(err){
     return res.status(500).send(err);
   }
 };
